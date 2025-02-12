c3133af59c14d0d1494039dd43af5e2ad777d296
Aluno <aluno@facens.br>
2025-02-12 20:37:03 -0300

    fim da aula1

--- Aula1A/Main_Intro.java
+++ Aula1A/Main_Intro.java
@@ -1,10 +1,15 @@
 public class Main_Intro {
     public static void main(String[] args) {
         mostrarAlgo("rafael");
+        soma(1,2);
     }
     
     public static void mostrarAlgo(String nome){
         System.out.println(nome); 
+    }
+    public static void soma(int num1, int num2){
+        int result = num1 + num2;
+        System.out.println("O resultado da soma e: "+result);
     }
 }
 
