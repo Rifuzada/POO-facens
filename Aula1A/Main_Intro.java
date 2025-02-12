caf3d5e1b5e0495c70fde548341fa7e4ad6b6eda
Aluno <aluno@facens.br>
2025-02-12 20:33:50 -0300

    fim da aula1

--- Aula1A/Main_Intro.java
+++ Aula1A/Main_Intro.java
@@ -1,10 +1,10 @@
 public class Main_Intro {
     public static void main(String[] args) {
-        mostrarAlgo();
+        mostrarAlgo("rafael");
     }
     
-    public static void mostrarAlgo(){
-        System.out.println("eu"); 
+    public static void mostrarAlgo(String nome){
+        System.out.println(nome); 
     }
 }
 
