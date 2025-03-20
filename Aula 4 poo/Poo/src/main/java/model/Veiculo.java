9e6ab5d9ab7e6010029368ecf43ec818cf71840a
Rafael <84077010+Rifuzada@users.noreply.github.com>
2025-03-19 22:05:02 -0300

    Update Veiculo.java

--- Aula 4 poo/Poo/src/main/java/model/Veiculo.java
+++ Aula 4 poo/Poo/src/main/java/model/Veiculo.java
@@ -1,17 +1,62 @@
 package model;
 
 public class Veiculo {
-    public String modelo;
-    public int potencia;
-    public String combustivel;
-    public String marca;
-    public int ano;
+    private String modelo;
+    private int potencia;
+    private String combustivel;
+    private String marca;
+    private int ano;
+    
+    public int getPotencia() {
+        return potencia;
+    }
+    public void setPotencia(int potencia) throws Exception {
+        if(potencia < 70){
+            throw new Exception("A potencia é invaida. ");
+        }
+        this.potencia = potencia;
+    }
+    public String getCombustivel() {
+        return combustivel;
+    }
+    public void setCombustivel(String combustivel) throws Exception {
+        if(combustivel != "Gasolina" && combustivel != "Diesel"){
+            throw new Exception("Combustivel nao aceito!");
+        }
+        this.combustivel = combustivel;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public int getAno() {
+        return ano;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public void setModelo(String modelo) throws Exception{
+        if( modelo == null || modelo.trim().isEmpty()){
+            throw new Exception("Modelo é obrigatorio. ");
+        }
+        this.modelo = modelo;
+    }
     public static void main(String[] args){
         Veiculo v = new Veiculo();
-        
-        v.modelo = "Comodoro";
-        v.marca = "Chevrolet";
-        v.potencia = 180;
-        v.combustivel = "Gasolina";
+        try {
+            v.setModelo("Comodoro");
+            v.setPotencia(60);
+            v.setCombustivel("Gasolina");
+            v.setAno(1977);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 }
