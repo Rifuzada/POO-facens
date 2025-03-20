ded662afeb3e0270c2861cf6d648ea6b0a10a19d
Rafael <84077010+Rifuzada@users.noreply.github.com>
2025-03-19 22:04:43 -0300

    Update Main.java

--- Aula 4 poo/Poo/src/main/java/main/Main.java
+++ Aula 4 poo/Poo/src/main/java/main/Main.java
@@ -3,20 +3,26 @@ import model.Veiculo;
 
 public class Main {
     public static void main(String[] args) {
-            Veiculo v = new Veiculo();
-            
-            v.modelo = "Comodoro";
-            v.marca = "Chevrolet";
-            v.potencia = 180;
-            v.combustivel = "Gasolina";
-            v.ano = 1977;
-            
-            System.out.println("Vendo este carro:");
-            System.out.println("Marca: " + v.marca);
-            System.out.println("Modelo: " + v.modelo);
-            System.out.println("Potencia: " + v.potencia);
-            System.out.println("Combustivel: " + v.combustivel);
-            System.out.println("Ano: " + v.ano);
+        Veiculo v = new Veiculo();
+
+        try {
+            v.setModelo("Comodoro");
+            v.setMarca("Chevrolet");
+            v.setCombustivel("Gasolina");
+            v.setAno(1977);
+            v.setPotencia(80);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Vendo este carro:");
+        System.out.println("Marca: " + v.getMarca());
+        System.out.println("Modelo: " + v.getModelo());
+        System.out.println("Potencia: " + v.getPotencia() + "cv");
+        System.out.println("Combustivel: " + v.getCombustivel());
+        System.out.println("Ano: " + v.getAno());
         
     }
 }
