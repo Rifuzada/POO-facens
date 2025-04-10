9fff1bc156cac777e19c0f2904c1e80da58892f1
Aluno <aluno@facens.br>
2025-04-09 21:59:39 -0300

    heranca

--- aulaheranca/src/main/java/main/TesteCarro.java
+++ aulaheranca/src/main/java/main/TesteCarro.java
@@ -24,7 +24,7 @@ class Carro extends Veiculo{
     Carro(String marca, String modelo){
         super(marca, modelo);
         this.ano = 0;
-        this.cor = "nao definida";
+        this.cor = "Nao informada";
 
     }
 
@@ -51,6 +51,7 @@ public class TesteCarro{
             String modeloDoCarro = scn.nextLine().trim();
             System.out.println("Deseja informar o ano e a cor? (S/N)");
             String resposta = scn.nextLine().trim().toLowerCase();
+            Carro meuCarro;
             if(resposta.equals("s")){
                 System.out.println("Digite o ano do carro: ");
                 int anoDoCarro = 0;
@@ -62,14 +63,20 @@ public class TesteCarro{
                 }
                 System.out.println("Digite a cor do carro: ");
                 String corDoCarro = scn.nextLine().trim();
-                Carro meuCarro = new Carro(marcaDoCarro, modeloDoCarro, anoDoCarro, corDoCarro);
-                meuCarro.exibirDetalhes();
+                meuCarro = new Carro(marcaDoCarro, modeloDoCarro, anoDoCarro, corDoCarro);
             }else{
-                Carro meuCarro = new Carro(marcaDoCarro, modeloDoCarro);
+                meuCarro = new Carro(marcaDoCarro, modeloDoCarro);
+            }
+            System.out.println("\nProcessando Informacoes...");
+            try {
+                Thread.sleep(1000);
                 meuCarro.exibirDetalhes();
+                scn.close();
+            } catch (InterruptedException e) {
+                System.err.println("Erro na pausa!");
             }
         } catch (Exception e) {
-        // TODO: handle exception
+            System.err.println("Erro");
         }
 
     }
